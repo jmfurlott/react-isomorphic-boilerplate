cf7b4f2aed7723587491dd08799afa63f0d52911
Joseph Furlott <jmfurlott@gmail.com>
2015-06-11 23:43:49 -0400

    ENH: Added hello world express server

---
+++ src/server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
