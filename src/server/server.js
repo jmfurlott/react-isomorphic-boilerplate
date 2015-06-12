e80a501e5b0d0eba8149124a90bc3bf7ac75b352
Joseph Furlott <jmfurlott@gmail.com>
2015-06-11 23:52:33 -0400

    ENH: Simple jade rendering working

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import React from "react";
+import Router from "react-router";
 const app = express();
 
+// set up Jade
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  res.render('index', { title: 'hey', message: 'world' });
 });
 
 var server = app.listen(3000, function () {
